8e43ad3c044ebd3462d7648da2e3171042c9c04c
Sagar Maheshwari <maheshwari.sagars2000@gmail.com>
2025-11-05 19:40:33 -0500

    Updated Architecture Diagram

--- med_agent/deploy.sh
+++ med_agent/deploy.sh
@@ -2,6 +2,5 @@ adk deploy cloud_run \
 --project=cloud-run-hackathon-475922 \
 --region=us-central1 \
 --service_name=medflow-agent-service \
---app_name=medflow-agent-app \
 --with_ui \
 ./med_agent
